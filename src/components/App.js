d8b01bcce0ada8c9145729cab8e2717c75fe3be2
Hernán D. Mercado <hernandmf@gmail.com>
2021-09-23 19:36:52 -0500

    Badge details

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import BadgeEdit from "../pages/BadgeEdit";
 import Badges from "../pages/Badges";
 import Layout from "./Layout";
 import { BrowserRouter, Route } from "react-router-dom";
+import BadgeDetails from "../pages/BadgeDetails";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/badges" component={Badges} />
         <Route exact path="/badges/new" component={BadgeNew} />
         <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
+        <Route exact path="/badges/:badgeId" component={BadgeDetails} />
       </Layout>
     </BrowserRouter>
   );
--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -22,7 +22,7 @@ class BadgesList extends React.Component {
             return (
               <li key={badge.id} className="BadgesListItem">
                 <Link
-                  to={`/badges/${badge.id}/edit`}
+                  to={`/badges/${badge.id}`}
                   className="text-reset text-decoration-none"
                 >
                   <Gravatar
